e59ea12d512d25542f5c074367a7ff17688baba9
Fikki Arsyi Nur Fadlilah <202010225026@mhs.ubharajaya.ac.id>
2022-10-12 18:32:10 +0700

    pembaruan pembayaran

--- oop_dasar.dart
+++ oop_dasar.dart
@@ -1,6 +1,7 @@
 class Person {
   var _name;
   var _address;
+  var _bayar;
 
   String getName() {
     return this._name;
@@ -17,14 +18,24 @@ class Person {
   void setAddress(String address) {
     this._address = address;
   }
+
+  int getpembayaran() {
+    return this._bayar;
+  }
+
+  void setpembayaran(int bayar) {
+    this._bayar = bayar;
+  }
 }
 
 // fungsi main
 main() {
-  var dian = new Person();
-  dian.setName("Dian");
-  dian.setAddress("Lombok");
+  var kry = new Person();
+  kry.setName("Dian");
+  kry.setAddress("Lombok");
+  kry.setpembayaran("30000");
 
-  print("Nama: ${dian.getName()}");
-  print("Alamat: ${dian.getAddress()}");
+  print("Nama: ${kry.getName()}");
+  print("Alamat: ${kry.getAddress()}");
+  print("pembayar: ${kry.getpembayaran()}");
 }
